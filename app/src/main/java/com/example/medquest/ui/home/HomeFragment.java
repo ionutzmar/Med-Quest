7a9d55b05ba7e713c7099da03ec82d4848732f91
Ionut Marius <ionutmarius3141@gmail.com>
2020-07-28 17:13:56 +0300

    minor UI improvements

--- app/src/main/java/com/example/medquest/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/medquest/ui/home/HomeFragment.java
@@ -8,9 +8,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -43,8 +41,6 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = root.findViewById(R.id.text_home);
 
-//        final String[] myDataset;
-//        ArrayList<String> myDataset;
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         final ArrayList<String> myDataset = new ArrayList<>();
         db.collection("clinicalCases")
@@ -55,24 +51,17 @@ public class HomeFragment extends Fragment {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Map<String, Object> clinicalCase = document.getData();
-//                                Log.d(TAG, (String) clinicalCase.get("description"));
-//                                textView.setText((String) clinicalCase.get("title"));
-//                                Log.d(TAG, document.getId() + " => " + document.getData());
 
                                 myDataset.add((String) clinicalCase.get("title"));
                             }
                             recyclerView = (RecyclerView) root.findViewById(R.id.my_recycler_view);
 
-                            // use this setting to improve performance if you know that changes
-                            // in content do not change the layout size of the RecyclerView
                             recyclerView.setHasFixedSize(true);
 
-                            // use a linear layout manager
                             layoutManager = new LinearLayoutManager(root.getContext());
                             recyclerView.setLayoutManager(layoutManager);
 
 
-                            // specify an adapter (see also next example)
                             mAdapter = new CasesAdapter(myDataset);
                             Log.d(TAG, myDataset.toString());
                             recyclerView.setAdapter(mAdapter);
